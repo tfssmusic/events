66dd62bb020fb033f372e2b768394826dc05dcda
jasonxwxia <jasonxwxia@gmail.com>
2026-03-05 12:33:24 -0500

    local files

---
+++ .eleventy.js
@@ -0,0 +1,8 @@
+export default async function(eleventyConfig) {
+  return {
+    dir: {
+      input: "src", // default value is "."
+      output: "_site" // is the default value
+    }
+  };
+};
---
+++ src/index.md
@@ -0,0 +1,9 @@
+---
+title: My Eleventy Portfolio
+layout: layout.njk
+---
+
+# Welcome to my Awesome Website
+
+
+Built with 11ty.
