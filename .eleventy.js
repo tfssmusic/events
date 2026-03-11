9a4f068b427003810237951c9909df508725c6ba
jasonxwxia <jasonxwxia@gmail.com>
2026-03-11 10:24:58 -0400

    updated layout

--- .eleventy.js
+++ .eleventy.js
@@ -2,12 +2,24 @@ export default async function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/style.css");
   eleventyConfig.addPassthroughCopy("src/assets");
 
+  // Add split filter
+  eleventyConfig.addFilter("split", function(str, separator) {
+    if (!str) return [];
+    return str.split(separator);
+  });
+
+  eleventyConfig.addFilter("readablePostDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: "Europe/Amsterdam",
+    }).setLocale('en').toLocaleString(DateTime.DATE_FULL);
+  });
+
+
   return {
     pathPrefix: "/events/",
     dir: {
-      input: "src", // default value is "."
-      output: "_site" // is the default value
+      input: "src",
+      output: "_site"
     }
   };
-};
-
+}
